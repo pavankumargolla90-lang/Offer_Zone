47286603525a9ecf085b12f73416d313338160e3
karthik muktham <mukthamkarthik@gmail.com>
2026-06-22 17:43:57 +0530

    Updated database

--- database/offer_zone.sql
+++ database/offer_zone.sql
@@ -216,6 +216,41 @@ COMMIT;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
 ;
 
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jun 22, 2026 at 02:08 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+SET
+  SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
+START TRANSACTION;
+
+SET
+  time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
+;
+
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+;
+
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+;
+
+/*!40101 SET NAMES utf8mb4 */
+;
+
+--
+-- Database: `offer_zone`
+--
+-- --------------------------------------------------------
+--
+-- Table structure for table `sliders`
+--
 CREATE TABLE `sliders` (
   `id` int(11) NOT NULL,
   `image` varchar(255) DEFAULT NULL,
@@ -271,3 +306,12 @@ MODIFY
   AUTO_INCREMENT = 3;
 
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+;
+
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
+;
+
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
+;
