a56321e14d757880239355eee8d8b683f3f6eaf5
sivaraj-12 <ssiva989499@gmail.com>
2026-06-23 10:37:41 +0530

    My slider changes

--- app/Http/Controllers/admin/SliderController.php
+++ app/Http/Controllers/admin/SliderController.php
@@ -1,73 +1,73 @@
-<?php
+    <?php
 
-namespace App\Http\Controllers\admin;
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Models\sliders;
+    namespace App\Http\Controllers\admin;
+    use App\Http\Controllers\Controller;
+    use Illuminate\Http\Request;
+    use App\Models\sliders;
 
-class SliderController extends Controller
-{
-    public function slider(Request $request)
+    class SliderController extends Controller
     {
-        $data = sliders::all();
-        return view('admin.sliders', compact('data'));
-    }
-    public function sliderstore(Request $request)
-    {
-        $slider = new sliders();
-        if ($request->hasFile('image')) {
-            $file = $request->file('image');
-            $filename = time() . '.' . $file->getClientOriginalExtension();
-            $file->move(public_path('Uploads/'), $filename);
-            $slider->image = $filename;
+        public function slider(Request $request)
+        {
+            $data = sliders::all();
+            return view('admin.sliders', compact('data'));
         }
-        $slider->title = $request->title;
-        $slider->heading = $request->heading;
-        $slider->save();
-        return redirect()->back()->with('success', 'Slider added successfully!');
+        public function sliderstore(Request $request)
+        {
+            $slider = new sliders();
+            if ($request->hasFile('image')) {
+                $file = $request->file('image');
+                $filename = time() . '.' . $file->getClientOriginalExtension();
+                $file->move(public_path('Uploads/'), $filename);
+                $slider->image = $filename;
+            }
+            $slider->title = $request->title;
+            $slider->heading = $request->heading;
+            $slider->save();
+            return redirect()->back()->with('success', 'Slider added successfully!');
 
-    }
-
-    public function editslider(Request $request, $id)
-    {
-        $data = sliders::find($id);
-        return view('admin.slidersedit', compact('data'));
-    }
-    public function updateslider(Request $request, $id)
-    {
-        $slider = sliders::find($id);
-        if ($request->hasFile('image')) {
-            $file = $request->file('image');
-            $filename = time() . '.' . $file->getClientOriginalExtension();
-            $file->move(public_path('Uploads/'), $filename);
-            $slider->image = $filename;
         }
-        $slider->title = $request->title;
-        $slider->heading = $request->heading;
-        $slider->save();
-        return redirect()->back()->with('success', 'Slider updated successfully!');
 
-    }
-    public function deleteslider($id)
-    {
-        $slider = sliders::find($id);
+        public function editslider(Request $request, $id)
+        {
+            $data = sliders::find($id);
+            return view('admin.slidersedit', compact('data'));
+        }
+        public function updateslider(Request $request, $id)
+        {
+            $slider = sliders::find($id);
+            if ($request->hasFile('image')) {
+                $file = $request->file('image');
+                $filename = time() . '.' . $file->getClientOriginalExtension();
+                $file->move(public_path('Uploads/'), $filename);
+                $slider->image = $filename;
+            }
+            $slider->title = $request->title;
+            $slider->heading = $request->heading;
+            $slider->save();
+            return redirect()->back()->with('success', 'Slider updated successfully!');
 
-        if ($slider) {
+        }
+        public function deleteslider($id)
+        {
+            $slider = sliders::find($id);
 
-            // Delete image from Uploads folder
-            if (
-                $slider->image &&
-                file_exists(public_path('Uploads/' . $slider->image))
-            ) {
+            if ($slider) {
 
-                unlink(public_path('Uploads/' . $slider->image));
+                // Delete image from Uploads folder
+                if (
+                    $slider->image &&
+                    file_exists(public_path('Uploads/' . $slider->image))
+                ) {
+
+                    unlink(public_path('Uploads/' . $slider->image));
+                }
+
+                // Delete database record
+                $slider->delete();
             }
 
-            // Delete database record
-            $slider->delete();
+            return redirect()->back()
+                ->with('success', 'Slider deleted successfully!');
         }
-
-        return redirect()->back()
-            ->with('success', 'Slider deleted successfully!');
     }
-}
